396c75546875f6127319e806f683479c5b729a77
olanetsoft <heedris2olubisi@gmail.com>
2020-07-26 16:28:25 +0100

    logic implemented

--- logic.js
+++ logic.js
@@ -1,27 +1,7 @@
 const url = require('url');
 
-exports.testRequest = function (req, res) {
-    body = '';
 
-    req.on('data', function (chunk) {
-        body += chunk;
-    });
-
-    req.on('end', function () {
-
-        postBody = JSON.parse(body);
-
-        var response = {
-            "text": "Post Request Value is  " + postBody.value
-        };
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
-    });
-};
-
-exports.sampleRequest = function (req, res) {
+exports.getRequest = function (req, res) {
     var response = {
         "data": "Hello World, Welcome to WeJapa Internships"
     };
@@ -30,3 +10,25 @@ exports.sampleRequest = function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(response));
 };
+
+
+exports.postRequest = function (req, res) {
+    let body = '';
+
+    req.on('data', function (chunkData) {
+        body += chunkData;
+    });
+
+    req.on('end', function () {
+        postBody = JSON.parse(body);
+        var response = {
+            "data": `Hello ${postBody.name}, Welcome to WeJapa Internships`
+        };
+
+        //send status code, set header and the response
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(response));
+    });
+};
+
--- main.js
+++ main.js
@@ -3,26 +3,16 @@ const url = require('url');
 
 module.exports = http.createServer((req, res) => {
 
-    var service = require('./logic.js');
+    const logic = require('./logic.js');
+
     const reqUrl = url.parse(req.url, true);
 
     // GET Endpoint
     if (reqUrl.pathname == '/' && req.method === 'GET') {
-        //logging to see output on console
-        console.log('Request Type:' +
-            req.method + ' Endpoint: ' +
-            reqUrl.pathname);
-
-        service.sampleRequest(req, res);
+        logic.getRequest(req, res);
 
         // POST Endpoint
     } else if (reqUrl.pathname == '/' && req.method === 'POST') {
-        //logging to see output on console
-        console.log('Request Type:' +
-            req.method + ' Endpoint: ' +
-            reqUrl.pathname);
-
-        service.testRequest(req, res);
-
+        logic.postRequest(req, res);
     }
 });
